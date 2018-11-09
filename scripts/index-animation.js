13d5d2e2f346901cc23a7e42fb1541c727fa1505
Dejan Bijelić <bijelic.99.dejan@hotmail.rs>
2018-11-10 00:51:08 +0100

    partialy implemented fullpage.js

--- scripts/index-animation.js
+++ scripts/index-animation.js
@@ -1,45 +1,57 @@
 $(document).ready(function () {
 
-    fixMobile()
-    
-    //$('nav').show();
-    navColorChange();
+
+
+
+
+    $('#wrapper').fullpage({
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        autoScrolling: true,
+        scrollOverflow: true,
+        scrollOverflowOptions: {
+            scrollbars: false
+        },
+        afterRender: function () {
+            
+        },
+        afterLoad: function (origin, destination, direction) {
+            pageAnimations(origin, destination, direction);
+        }
+    });
+
     navbarShowHide();
 
-    doAnimation("#msg", "animated bounceIn")
-    addScreenEnterAnimationOnElement("#msg", "animated bounceIn", "10");
-    addScreenEnterAnimationOnElement(".social-container-container", "animated bounceIn", "90");
-    addScreenEnterAnimationOnElement(".description-container", "animated bounceIn", "90");
-    //addScreenEnterAnimationOnElement("#contactMe", "animated bounceIn");
-    
 
 
-   
-    
+
+
+
+
+
 
 });
 
-function fixMobile(){
+function fixMobile() {
 
     var pr = window.parent.document;
-    $('meta',pr).attr('name','viewport');
-    $('meta',pr).attr('content','width=device-width');
-    $('meta',pr).attr('initial-scale','1.0');
+    $('meta', pr).attr('name', 'viewport');
+    $('meta', pr).attr('content', 'width=device-width');
+    $('meta', pr).attr('initial-scale', '1.0');
     console.log(pr);
 
 }
 
-function navbarShowHide(){
-    var scrollPos=0;
+function navbarShowHide() {
+    var scrollPos = 0;
 
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         var curPos = $(window).scrollTop();
-        if(curPos > scrollPos){
+        if (curPos > scrollPos) {
             $("nav").removeClass("animated slideOutUp slideInDown fast").addClass("animated slideOutUp fast");
-            
-            
+
+
         }
-        else{
+        else {
             $("nav").removeClass("animated slideOutUp slideInDown fast").addClass("animated slideInDown fast");
         }
         scrollPos = curPos;
@@ -52,55 +64,20 @@ function navColorChange() {
 
     about.waypoint(function () {
         switchNavColor();
-    },{offset: '10%'});
+    }, { offset: '10%' });
 }
-function switchNavColor() {
-    var color1 = "rgba(0, 0, 0, 0)"
-    var color2 = "rgb(86, 82, 225)"
-    var nav = $('.navbar-custom');
 
-    if (nav.css("background-color") === color1) {
-        nav.css("background-color", color2,);
+function pageAnimations(origin, destination, direction) {
+
+    if(origin === null || origin.index=== 0){
+        
+        
+        doAnimation("#msg", "animated bounceIn");
     }
-    else {
-        nav.css("background-color", color1);
-    }
+    console.log(origin.index)
 }
 
-function addScreenEnterAnimationOnElement(ElementIdOrClass, Animation, Offset) {
-    var element = $(ElementIdOrClass);
-    var animationEnd = "animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd";
-
-    var downWPT = new Waypoint({
-        element: element,
-        handler: function (direction) {
-            if (direction === "down") {
-                element.addClass(Animation).one(animationEnd, function () {
-                    element.removeClass(Animation);
-                });
-        }
-        },
-        offset: Offset
-    });
-
-    var upWPT = new Waypoint({
-        element: element,
-        handler: function (direction) {
-            
-            if (direction === "up") {
-                element.addClass(Animation).one(animationEnd, function () {
-                    element.removeClass(Animation);
-                });
-            }
-        },
-        offset: '-'+Offset
-    });
-
-
-
-}
-
-function doAnimation(ElementIdOrClass, Animation){
+function doAnimation(ElementIdOrClass, Animation) {
     var animationEnd = "animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd";
     var element = $(ElementIdOrClass);
 
